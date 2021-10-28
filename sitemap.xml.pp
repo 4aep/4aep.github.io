76c59239f9d6e00894d11bce80752fb3c1b8509e
4aep <swbvty@gmail.com>
2021-10-27 19:17:55 -0700

    add sitemap
    vital for SEO and indexing. It also functions as a ad hoc RSS feed.

---
+++ sitemap.xml.pp
@@ -0,0 +1,30 @@
+#lang pollen
+
+◊(require racket/string)
+◊(require pollen/pagetree)
+
+◊(define pagelist (pagetree->list (get-pagetree "index.ptree")))
+
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+<url>
+<loc>https://4aep.github.io</loc>
+<changefreq>daily</changefreq>
+</url>
+
+<url>
+<loc>https://4aep.github.io/site-index.html</loc>
+<changefreq>daily</changefreq>
+</url>
+
+◊(apply string-append (map (lambda (x)
+                             (string-join `(,(format "<url>\n<loc>https://4aep.github.io/~a</loc>\n" x)
+                                            ,(if (select 'edited-date x) (format "<lastmod>~a</lastmod>\n" (select 'edited-date x))
+                                                 (if (select 'original-date x) (format "<lastmod>~a</lastmod>\n" (select 'original-date x)) ""))
+                                            "<changefreq>weekly</changefreq>\n</url>\n\n")
+                                          ""
+                                          ))
+                           pagelist))
+
+</urlset>
